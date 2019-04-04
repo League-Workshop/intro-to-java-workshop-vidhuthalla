3845b7b298ca50e22d88fc7b69a70ad046a92569
vidhuthalla <48264137+vidhuthalla@users.noreply.github.com>
2019-04-03 18:30:39 -0700

    Day 5
    :smile:

--- src/section4/DragonFight.java
+++ src/section4/DragonFight.java
@@ -12,29 +12,36 @@ public class DragonFight {
 		
 		JOptionPane.showMessageDialog(null, "Defeat the dragon to take its treasure!", "Dragon Fighter", 0, dragon);
 		// 2. Create a variable called "playerHealth" to store your health (set it equal to 100)
-	
+	int playerHealth = 100;
 		// 3. Create a variable called "dragonHealth" to store the dragon's health (set it equal to 100)
-		
+	int dragonHealth = 100;
+
 		// 4. Create a variable to hold the damage the player's attack does each round
-		
+	int playerDamage = 0;
+
 		// 5. Create a variable to hold the damage the dragon's attack does each round
+	int dragonDamage = 0;
 		
 		
 		// 6.  Delete the slashes at the beginning of the next line.  
-		//while(playerHealth>0 && dragonHealth>0) {    //this line of code keeps the battle going until someone's health reaches 0 
+		while(playerHealth>0 && dragonHealth>0) {    //this line of code keeps the battle going until someone's health reaches 0 
 		
 		// 7. Add a closing mustache at the very bottom of this program (since we just added an opening mustache on the previous step).
 		
 		// 8. Ask the player in a pop-up if they want to attack the dragon with a yell or a kick
-		
+			String YellorKick = JOptionPane.showInputDialog(null, "Yell or Kick");
 		// 9. If they typed in "yell":
-		
+			 if (YellorKick.equals("Yell")) {dragonDamage = new Random().nextInt(11);
+			 dragonHealth -= dragonDamage;
+			 }
 			//-- Find a random number between 0 and 10 and store it in dragonDamage
 			
 			//-- Subtract that number from the dragon's health variable 
 			
 		// 10. If they typed in "kick":
-		
+			 if (YellorKick.equals("Kick")) {dragonDamage = new Random().nextInt(26);
+			 dragonHealth -= dragonDamage;
+			 }
 			//-- Find a random number between 0 and 25 and store it in dragonDamage
 			
 			//-- Subtract that number from the dragon's health variable
@@ -42,24 +49,27 @@ public class DragonFight {
 			
 		
 		// 11.  Find a random number between 0 and 35 and store it in playerDamage
-		
-		// 12. Subtract this number from the player's health
+			  playerDamage = new Random().nextInt(36);
+			 playerHealth -= playerDamage;
+			 
+	// 12. Subtract this number from the player's health
 		
 		
 		
 		// 13. If the user's health is less than or equal to 0
-		
+		if ( playerHealth<0) {	JOptionPane.showMessageDialog(null,"YOU LOST");}
 			//-- Tell the user that they lost
 			
 		
 		// 14. Else if the dragon's health is less than or equal to 0
 		
 			//-- Tell the user that the dragon is dead and they took a ton of gold!
-			
+		if ( dragonHealth<0) {	JOptionPane.showMessageDialog(null,"YOU WON");}
+
 	   //  15.  Else
-			
+		JOptionPane.showMessageDialog(null,"Your health is " + playerHealth + " The dragons health is " + dragonHealth);
 			//-- Pop up a message that tells the their current health and the dragon's currently health (Bonus: Also display the amount of health that was lost for each player this round)
 			
+}
+			 }		 
 		}
-	}
-
